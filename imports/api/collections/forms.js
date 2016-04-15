26da85a0472fd19c7670101d7f2bb5ee2c00dc1f
u <ucekmez@gmail.com>
2016-04-15 19:54:35 +0300

    get the form responses

--- imports/api/collections/forms.js
+++ imports/api/collections/forms.js
@@ -1,1 +1,27 @@
 export const Forms = new Mongo.Collection('forms');
+
+export const FormResponses = new Mongo.Collection('form_responses');
+
+FormResponses.attachSchema(new SimpleSchema({
+  form: { type: String, max: 64 },
+  user: { type: String, max: 64},
+  response: { type: Object },
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert) { return new Date(); }
+      else if (this.isUpsert) { $setOnInsert: new Date(); }
+      else { this.unset(); }
+    }
+  },
+  updatedAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isUpdate) {
+        return new Date();
+      }
+    },
+    denyInsert: true,
+    optional: true
+  }
+}));
--- imports/ui/company/forms/forms.js
+++ imports/ui/company/forms/forms.js
@@ -7,6 +7,8 @@ import './preview_form.html'; // CompanyPreviewForm
 
 
 
+//////////////////////// ******************** CompanyEditForm
+
 Template.CompanyEditForm.onRendered(function() {
   FlowRouter.subsReady("getformpreview", function() {
     const form = Forms.findOne({_id: FlowRouter.getParam('formId')});
@@ -32,6 +34,8 @@ Template.CompanyEditForm.onRendered(function() {
 });
 
 
+//////////////////////// ******************** CompanyListForms
+
 Template.CompanyListForms.helpers({
   forms() {
     return Forms.find({ user: Meteor.userId() },{ sort: { createdAt: -1}})
@@ -49,11 +53,50 @@ Template.CompanyListForms.events({
 });
 
 
+//////////////////////// ******************** CompanyPreviewForm
+
+Template.CompanyPreviewForm.events({
+  'click #submit-button'(event, instance) {
+    const fields = JSON.parse(Frm.payload).fields;
+
+    const response = new Array();
+
+    fields.forEach(function(field) {
+      if (field.field_type === 'paragraph') {
+        const text = $(`.paragraph input[name=${field.cid}]`).val();
+        response.push({cid: field.cid, val: text});
+      } else if (field.field_type === 'checkbox') {
+        const checks = $(`.checkbox input[name=${field.cid}]:checked`).map(function() {
+          return this.value;
+        }).get();
+        response.push({cid: field.cid, val: checks});
+      } else if (field.field_type === 'range') {
+        const range = $(`.range input[name=${field.cid}]`).val();
+        response.push({cid: field.cid, val: range});
+      } else if (field.field_type === 'address') {
+        const address = $(`.address input[name=${Fields[3].cid}_address]`).val();
+        const city    = $(`.address input[name=${Fields[3].cid}_city]`).val();
+        const country = $(`.address input[name=${Fields[3].cid}_country]`).val();
+        response.push({cid: field.cid, val: [{address: address, city: city, country: country}]});
+      } else if (field.field_type === 'radio') {
+        const selected = $(`.radio input[name=${field.cid}]:checked`).val();
+        response.push({cid: field.cid, val: selected});
+      } else if (field.field_type === 'dropdown') {
+        const drop_select = $(`.dropdown select[name=${field.cid}]`).val();
+        response.push({cid: field.cid, val: drop_select});
+      } else if (field.field_type === 'number') {
+        const number = $(`.number input[name=${field.cid}]`).val();
+        response.push({cid: field.cid, val: number});
+      }
+    });
+
+  }
+});
+
 Template.CompanyPreviewForm.helpers({
   form() {
-    const frm = Forms.findOne({ _id: FlowRouter.getParam('formId') });
-    Frm = frm;
-    return frm;
+    Frm = Forms.findOne({ _id: FlowRouter.getParam('formId') });
+    return Frm;
   }
 });
 
